9b127c2fbb2dd8c35a6d991b3e4f2f4ee5f3ef9d
pjayaprakash1510 <118723199+pjayaprakash1510@users.noreply.github.com>
2024-07-24 16:57:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bitter: ["Bitter", "serif"],
+      },
+      fontWeight: {
+        100: "100",
+        200: "200",
+        300: "300",
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+        800: "800",
+        900: "900",
+      },
+    },
+  },
+
+  plugins: [],
+});
